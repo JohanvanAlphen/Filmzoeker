6a63bfb1ba429a03333f4d9cd06a44047cb8c0a6
Johan van Alphen <johanvanalphen@hotmail.com>
2021-02-23 16:42:44 +0100

    Mijn eindopdracht voor Filmzoeker

--- script.js
+++ script.js
@@ -1,49 +1,67 @@
-// Deel 1; Elementen toevoegen
+const radioBtns = document.getElementsByName("film-filter");
+const resetBtn = document.getElementById("reset");
+const searchBar = document.getElementById("searchbar");
+const movieList = document.getElementById("movie-list");
 
-const bigFiveList = document.querySelector(".big-five-list");
-const spottedList = document.querySelector(".spotted-animals-list");
+const addMoviesToDom = (movieSelection) => {
+    const movieListLi = movieSelection.map(movie => {
+        const newLi = document.createElement("li");
+        const newA = document.createElement("a");
+        const newImage = document.createElement("img");
+        newLi.appendChild(newA);
+        newA.appendChild(newImage);
+        const moviePoster = movie.Poster;
+        const posterId = movie.imdbID;
+        newA.setAttribute("href", "https://www.imdb.com/title/" + posterId);
+        newImage.src = moviePoster;
+        return newLi;
+    });
+    movieListLi.forEach(movieSelection => {
+        movieList.appendChild(movieSelection);
+    });
+};
+addMoviesToDom(movies);
 
-bigFiveList.addEventListener("click", alertText,);
+function addEventListeners() {
 
-function alertText(e) {
-    if (e.target !== e.currenTarget) {
-        const clickedItem = e.target.id;
-        alert("You have seen a " + clickedItem);
-        const newListItem = document.createElement("li");
-        newListItem.appendChild(document.createTextNode(clickedItem));
-        const list = document.getElementById("spotted-animals-list");
-        list.appendChild(newListItem).className = "spotted-animals-list-item";
+    radioBtns.forEach(radio => {
+        radio.addEventListener("change", (event) => {
+            handleOnChangeEvent(event.target.id);
+        });
+    });
+    resetBtn.addEventListener("click", () => {
+        reset();
+        searchBar.value = "";
+        addMoviesToDom(movies);
+    });
+    searchBar.addEventListener("keyup", (event) => {
+        handleOnChangeEvent(event.target.value.toLowerCase());
+    });
+}
+addEventListeners();
+
+const handleOnChangeEvent = (event) => {
+    let filteredMovies = movies
+    switch (event) {
+        case "X-Men":
+        case "Avengers":
+        case "Princess":
+        case "Batman":
+            filteredMovies = movies.filter(movie => movie.Title.includes(event));
+            break;
+        case "newestMovies":
+            filteredMovies = movies.filter(movie => movie.Year > 2013);
+            break;
+        case "allMovies":
+            addMoviesToDom(movies);
+            break;
+        default:
+            filteredMovies = movies.filter(movie => movie.Title.toLowerCase().includes(event));
     };
+    reset();
+    addMoviesToDom(filteredMovies);
 };
 
-// Deel 2; 1 element verwijderen 
-
-const deleteFirstItem = document.getElementById("remove-first-item-button");
-
-deleteFirstItem.addEventListener("click", removeFirstItem);
-
-function removeFirstItem() {
-
-    const firstItem = document.getElementById("spotted-animals-list");
-
-    const firstLi = firstItem.getElementsByTagName("li")[0];
-    firstItem.removeChild(firstLi);
-    console.log(firstItem);
-
-    // Alternatief:
-    // const firstItem = document.getElementById("spotted-animals-list");
-    // firstItem.removeChild(firstItem.childNodes[0]);
-}
-
-// Deel 3; Meerdere element verwijderen uit de DOM
-
-const deleteAllItems = document.getElementById("remove-all-button");
-
-deleteAllItems.addEventListener("click", deleteAll);
-
-function deleteAll() {
-    const spottedAnimalList = document.getElementById("spotted-animals-list");
-    spottedAnimalList.querySelectorAll("*").forEach(n => n.remove());
-    // Alternatief:
-    // spottedAnimalList.innerHTML = "";
+const reset = () => {
+    movieList.innerHTML = "";
 }
